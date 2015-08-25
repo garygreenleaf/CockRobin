957dc0563dadaa44352304ece4af41d2dd2f142a
Gary Greenleaf <gary@fantasticthinking.com>
2015-08-25 15:27:07 +0100

    style updates

--- wp-content/themes/wpex-fabulous-cr/partials/logo.php
+++ wp-content/themes/wpex-fabulous-cr/partials/logo.php
@@ -35,17 +35,18 @@ $fb_logo      = get_template_directory_uri(). '/images/findus.png';
 			<?php } ?>
 		</div>
 	<?php } ?>
-</div><!-- #logo -->
+</div>
+<!-- #logo -->
+<div class="span_1_of_2 col col-2">
+  <a href="http://www.facebook.com/cockrobinanglais" target="_blank"><img class="fb_logo" src="<?php echo $fb_logo;?>"></a>
+</div>
 <!--
 <div id="search-box" class="span_1_of_2 col col-2">
     <form method="get" id="searchform" class="span_2_of_3 col first col-2" action="<?php get_bloginfo('url'); ?>/">
         <div class="form-inner">
-        	<input type="text" size="18" value="<?php echo esc_html($s, 1); ?>" placeholder="Search" name="s" id="s" />
+          <input type="text" size="18" value="<?php echo esc_html($s, 1); ?>" placeholder="Search" name="s" id="s" />
             <input type="submit" id="searchsubmit" value="Search" class="btn" />
         </div>
     </form>
 </div>
 -->
-<div class="span_1_of_2 col col-2">
-  <a href="http://www.facebook.com/cockrobinanglais" target="_blank"><img class="fb_logo" src="<?php echo $fb_logo;?>"></a>
-</div>
