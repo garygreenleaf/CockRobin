99113b44a51989be0a7797b02c854c35638278dd
Gary Greenleaf <g.greenleaf@catchdigital.com>
2016-09-18 08:56:03 +0100

    create develop branch

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'cockrobin');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'brabantia');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
